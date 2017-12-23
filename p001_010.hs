a1942debc8498ee320988c33151be34e57cc554c
Jaan Tollander de Balsch <jaantollander@gmail.com>
2017-12-23 12:25:57 +0200

    Problem 10 solved

--- p001_010.hs
+++ p001_010.hs
@@ -105,3 +105,13 @@ pack list = pack' $ split' ([], list)
   where
     pack' (h, []) = [h]
     pack' (h, t) = [h] ++ pack(t)
+
+
+-- Problem 10
+-- Run-length encoding of a list. Use the result of problem P09 to implement the
+-- so-called run-length encoding data compression method. Consecutive duplicates
+-- of elements are encoded as lists (N E) where N is the number of duplicates of
+-- the element E.
+encode :: Eq a => [a] -> [(Int, a)]
+encode [] = error ""
+encode list = map (\x -> (length x, head x)) (pack list)
