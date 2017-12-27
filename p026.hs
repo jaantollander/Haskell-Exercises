040b46e11cc325535e564eb6fb0adaff7c077aa8
Jaan Tollander de Balsch <jaantollander@gmail.com>
2017-12-27 22:54:24 +0200

    Problem 27 unfinished

--- p026.hs
+++ p026.hs
@@ -1,12 +1,10 @@
 -- Problem 26
 -- Generate the combinations of K distinct objects chosen from the N elements
 -- of a list
--- https://en.wikipedia.org/wiki/Combination
 combinations :: Int -> [a] -> [[a]]
 combinations _ [] = error ""
 combinations k list@(x:xs)
   | k == 0 = [[]]
-  | k == 1 = [[e] | e <- list]
   | k == l = [list]
   | k >= l = error ""
   | otherwise = [x:c | c <- (combinations (k - 1) xs)] ++ (combinations k xs)
--- p027.hs
+++ p027.hs
@@ -1,11 +1,19 @@
 -- Problem 27
 -- Group the elements of a set into disjoint subsets.
+
+-- TODO:
 combinations_with_residuals :: Int -> [a] -> [([a], [a])]
-combinations_with_residuals k list = ...
+combinations_with_residuals k list = [([], [])]
 
 -- [2, 2, 5], "ABCDEFGHI" ->
+-- [ [[A, B], [C, D, E], [F, G, H, I, J]], ... ]
+
 -- FIXME:
 group :: [Int] -> [a] -> [[[a]]]
+group [] _ = [[[]]]
+group _ [] = [[[]]]
 group (k:ks) list =
-  [combination:(group ks unused) |
-   (combination, unused) <- (combinations_with_residuals k list)]
+  foldr [] (++) $ map (group' ks) (combinations_with_residuals k list)
+  where
+    group' :: [Int] -> ([a], [a]) -> [[[a]]]
+    group' ks (combination, residual) = map (:combination) (group ks residual)
